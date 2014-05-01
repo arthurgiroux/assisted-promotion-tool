112c6a8b27cd5c7c22ec70418a40b35cb1ab1780
Valentin Matter <valentin.matter@epfl.ch>
2014-05-01 16:24:08 +0200

    4

--- src/recommender/WebServer.java
+++ src/recommender/WebServer.java
@@ -20,7 +20,7 @@ public class WebServer {
 
   public static void main(String[] args) throws Exception {
     HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-    server.createContext("/recommand", new MyHandler());
+    server.createContext("/recommend", new MyHandler());
     server.setExecutor(null); // creates a default executor
     server.start();
   }
