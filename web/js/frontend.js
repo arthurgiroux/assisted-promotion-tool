807c8702166da50922704575a415e8f103561611
Valentin Matter <valentin.matter@epfl.ch>
2014-05-13 21:14:54 +0200

    Small improvement in the frontend

--- web/js/frontend.js
+++ web/js/frontend.js
@@ -5,67 +5,116 @@ $(document).ready(function () {
 	var events = {
 		"days_countdown" : {
 			"title" : "Title days_countdown",
-			"text" : "Text days_countdown"
+			"text" : "Text days_countdown",
+			"icon" : "time",
+			"color" : "#FFFFFF",
+			"bgcolor" : "#000000"
 		},
 		"days_video_clip" : {
 			"title" : "Title days_video_clip",
-			"text" : "Text days_video_clip"
+			"text" : "Text days_video_clip",
+			"icon" : "facetime-video",
+			"color" : "#FFFFFF",
+			"bgcolor" : "#000000"
 		},
 		"days_press_campaign" : {
 			"title" : "Title days_press_campaign",
-			"text" : "Text days_press_campaign"
+			"text" : "Text days_press_campaign",
+			"icon" : "comment",
+			"color" : "#FFFFFF",
+			"bgcolor" : "#000000"
 		},
 		"days_presale_campaign" : {
 			"title" : "Title days_presale_campaign",
-			"text" : "Text days_presale_campaign"
+			"text" : "Text days_presale_campaign",
+			"icon" : "calendar",
+			"color" : "#FFFFFF",
+			"bgcolor" : "#000000"
 		},
 		"days_interview" : {
 			"title" : "Title days_interview",
-			"text" : "Text days_interview"
+			"text" : "Text days_interview",
+			"icon" : "bullhorn",
+			"color" : "#FFFFFF",
+			"bgcolor" : "#000000"
 		},
 		"days_single_release" : {
 			"title" : "Title days_single_release",
-			"text" : "Text days_single_release"
+			"text" : "Text days_single_release",
+			"icon" : "music",
+			"color" : "#FFFFFF",
+			"bgcolor" : "#000000"
 		},
 		"days_cd_release_show" : {
 			"title" : "Title days_cd_release_show",
-			"text" : "Text days_cd_release_show"
+			"text" : "Text days_cd_release_show",
+			"icon" : "eject",
+			"color" : "#FFFFFF",
+			"bgcolor" : "#000000"
 		},
 		"days_first_tweet" : {
 			"title" : "Title days_first_tweet",
-			"text" : "Text days_first_tweet"
+			"text" : "Text days_first_tweet",
+			"icon" : "bullhorn",
+			"color" : "#FFFFFF",
+			"bgcolor" : "#00a8ff"
 		},
 		"days_first_fb" : {
 			"title" : "Title days_first_fb",
-			"text" : "Text days_first_fb"
+			"text" : "Text days_first_fb",
+			"icon" : "thumbs-up",
+			"color" : "#FFFFFF",
+			"bgcolor" : "#002ec6"
 		},
 		"days_announcement" : {
 			"title" : "Title days_announcement",
-			"text" : "Text days_announcement"
+			"text" : "Text days_announcement",
+			"icon" : "bullhorn",
+			"color" : "#FFFFFF",
+			"bgcolor" : "#000000"
 		},
 		"days_album_cover" : {
 			"title" : "Title days_album_cover",
-			"text" : "Text days_album_cover"
+			"text" : "Text days_album_cover",
+			"icon" : "picture",
+			"color" : "#FFFFFF",
+			"bgcolor" : "#000000"
 		},
 		"days_teaser" : {
 			"title" : "Title days_teaser",
-			"text" : "Text days_teaser"
+			"text" : "Text days_teaser",
+			"icon" : "film",
+			"color" : "#FFFFFF",
+			"bgcolor" : "#000000"
+		},
+		"release_date" : {
+			"title" : "Release date",
+			"text" : "Text blabla",
+			"icon" : "ok",
+			"color" : "#FFFFFF",
+			"bgcolor" : "#23d300"
 		}
 	};
 
 	var createTimelineElement = function (data, inverted) {
+		var event = events[data.event];
+
 		inverted = typeof inverted !== 'undefined' ? inverted : false;
 		var element = inverted ? $('<li class="timeline-inverted"></li>') : $('<li></li>');
 		var panel = $('<div class="timeline-panel">');
 		var panelHeading = $('<div class="timeline-heading"></div>');
 		var panelBody = $('<div class="timeline-body"></div>');
 
-		element.append('<div class="timeline-badge"><i class="glyphicon glyphicon-bullhorn"></i></div>');
-		panelHeading.append('<h4 class="timeline-title">' + data.title + '</h4>');
+		var timelineBadge = $('<div class="timeline-badge"></div>');
+		timelineBadge.css('color', event.color);
+		timelineBadge.css('background-color', event.bgcolor);
+		timelineBadge.append('<i class="glyphicon glyphicon-' + event.icon + '"></i>');
+		element.append(timelineBadge);
+		panelHeading.append('<h4 class="timeline-title">' + event.title + '</h4>');
 
 		var dateString = months[data.date.getMonth()] + " " + data.date.getDate() + ", " + data.date.getFullYear();
 		panelHeading.append('<p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> on ' + dateString + '</small></p>');
-		panelBody.append('<p>' + data.message + '</p>');
+		panelBody.append('<p>' + event.text + '</p>');
 
 		element.append(panel);
 		panel.append(panelHeading);
@@ -131,17 +180,15 @@ $(document).ready(function () {
 				var d = new Date(releaseDate);
 				d.setDate(d.getDate() - Math.round(obj.days));
 				$('#timelineContent').append(createTimelineElement({
-					title : events[obj.event].title,
-					message : events[obj.event].text,
+					event : obj.event,
 					date : d
 				}, i%2 == 1));
 			}
 
 			$('#timelineContent').append(createTimelineElement({
-				title : "Release date",
-				message : "Test",
+				event : "release_date",
 				date : releaseDate
-			}));
+			}, data.results.length%2 == 1));
 
 			// Displays the timeline
 			$('#timelineContainer').fadeIn();
