17f113b4cdc540ed1bb3fe27eb5ab8afd961e38a
Arthur Giroux <arthur.giroux@epfl.ch>
2014-05-19 18:28:38 +0200

    good titles

--- web/js/frontend.js
+++ web/js/frontend.js
@@ -4,8 +4,10 @@ $(document).ready(function () {
 
 	var events = {
 		"days_countdown" : {
-			"title" : "Countdown",
-			"text" : "Countdown before your album release:<br />i.e: <b>Only 15 days before our new album ! Keep tuned !</b>",
+			"title" : "Countdown before the release",
+			"text" : "<blockquote>
+  				<p>Only 15 days before our new album ! Stay tuned !</p>
+				</blockquote>",
 			"icon" : "time",
 			"color" : "#FFFFFF",
 			"bgcolor" : "#000000"
@@ -19,77 +21,99 @@ $(document).ready(function () {
 		},
 		"days_press_campaign" : {
 			"title" : "Press campaign",
-			"text" : "Talk about your ongoing press campaign.<br />i.e: <b>Check us in this new press article in the Guardian.</b>",
+			"text" : "<blockquote>
+  				<p>Check us in this new press article in the Guardian.</p>
+				</blockquote>",
 			"icon" : "comment",
 			"color" : "#FFFFFF",
 			"bgcolor" : "#000000"
 		},
 		"days_presale_campaign" : {
 			"title" : "Presale campaign",
-			"text" : "i.e: <b>Preorder our new album now for only 10.- !</b>",
+			"text" : "<blockquote>
+  				<p>Preorder our new album now for only 10.- !</p>
+				</blockquote>",
 			"icon" : "calendar",
 			"color" : "#FFFFFF",
 			"bgcolor" : "#000000"
 		},
 		"days_interview" : {
-			"title" : "Title days_interview",
-			"text" : "Text days_interview",
+			"title" : "Interview",
+			"text" : "<blockquote>
+  				<p>Check our interview tonight on the Late Night Show !</p>
+				</blockquote>",
 			"icon" : "bullhorn",
 			"color" : "#FFFFFF",
 			"bgcolor" : "#000000"
 		},
 		"days_single_release" : {
-			"title" : "Title days_single_release",
-			"text" : "Text days_single_release",
+			"title" : "Release of a single",
+			"text" : "<blockquote>
+  				<p>We released the single &lquote; Big Data &rquote; of the album &lquote; Pretty big things &rquote; Check it out !</p>
+				</blockquote>",
 			"icon" : "music",
 			"color" : "#FFFFFF",
 			"bgcolor" : "#000000"
 		},
 		"days_cd_release_show" : {
-			"title" : "Title days_cd_release_show",
-			"text" : "Text days_cd_release_show",
+			"title" : "CD release show",
+			"text" : "<blockquote>
+  				<p>Tonight to celebrate the release of our CD we are doing a show at Hyde Park</p>
+				</blockquote>",
 			"icon" : "eject",
 			"color" : "#FFFFFF",
 			"bgcolor" : "#000000"
 		},
 		"days_first_tweet" : {
-			"title" : "Title days_first_tweet",
-			"text" : "Text days_first_tweet",
+			"title" : "First tweet about the album",
+			"text" : "<blockquote>
+  				<p>We are working very hard on our new album.</p>
+				</blockquote>",
 			"icon" : "bullhorn",
 			"color" : "#FFFFFF",
 			"bgcolor" : "#00a8ff"
 		},
 		"days_first_fb" : {
-			"title" : "Title days_first_fb",
-			"text" : "Text days_first_fb",
+			"title" : "First facebook post about the album",
+			"text" : "<blockquote>
+  				<p>Stay tuned for our new album very soon !</p>
+				</blockquote>",
 			"icon" : "thumbs-up",
 			"color" : "#FFFFFF",
 			"bgcolor" : "#002ec6"
 		},
 		"days_announcement" : {
-			"title" : "Title days_announcement",
-			"text" : "Text days_announcement",
+			"title" : "Announcement about the album",
+			"text" : "<blockquote>
+  				<p>We are proud to announce the making of our new album.</p>
+				</blockquote>",
 			"icon" : "bullhorn",
 			"color" : "#FFFFFF",
 			"bgcolor" : "#000000"
 		},
 		"days_album_cover" : {
-			"title" : "Title days_album_cover",
-			"text" : "Text days_album_cover",
+			"title" : "Album cover artwork",
+			"text" : "<blockquote>
+  				<p>Check our amazing cover artwork made by the great artist Lolita Mujer.</p>
+				</blockquote>",
 			"icon" : "picture",
 			"color" : "#FFFFFF",
 			"bgcolor" : "#000000"
 		},
 		"days_teaser" : {
-			"title" : "Title days_teaser",
-			"text" : "Text days_teaser",
+			"title" : "Teaser",
+			"text" : "<blockquote>
+  				<p>Today we released a teaser for our new album, check it out !</p>
+				</blockquote>",
 			"icon" : "film",
 			"color" : "#FFFFFF",
 			"bgcolor" : "#000000"
 		},
 		"release_date" : {
 			"title" : "Release date",
-			"text" : "Text blabla",
+			"text" : "<blockquote>
+  				<p>Our album &lquote; Big Data &rquote; will be release on the 20th of May !</p>
+				</blockquote>",
 			"icon" : "ok",
 			"color" : "#FFFFFF",
 			"bgcolor" : "#23d300"
@@ -177,7 +201,7 @@ $(document).ready(function () {
 
 			$('#statsContent').empty();
 
-			var statsText = "This timeline was computed by aggregating the promotion timeline of " + data.stats.artists_count + " artists (avg. similarity : " + data.stats.average_sim + "). <br />";
+			var statsText = "This timeline was computed by aggregating the promotion timeline of " + data.stats.artists_count + " artists"; //(avg. similarity : " + data.stats.average_sim + "). <br />";
 			statsText += "Average similarity over all the artists of our database : " + data.stats.average_sim_overall + "<br />";
 			statsText += "Artist best match : " + data.stats.best_match + "<br />";
 			$('#statsContent').html(statsText);
